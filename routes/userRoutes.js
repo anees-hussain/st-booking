3a6da1954fe05be1e2e92c4a0818806dbf8d8535
Anees Hussain <aneeshussain334@gmail.com>
2026-06-27 03:21:59 +0500

    added deliveryman route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -158,6 +158,21 @@ router.get("/sellers", async (req, res) => {
   }
 });
 
+// GET ALL Delivery Men publicly accessible
+router.get("/delivery-men", async (req, res) => {
+  try {
+    const users = await User.find({ designation: "deliveryman" }).select(
+      "fullName _id",
+    );
+
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
 // GET SINGLE USER
 router.get("/:id", protect, async (req, res) => {
   try {
